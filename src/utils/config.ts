a07fce70bcec63c36ac3e7fa162abc9bbdb95559
Matt Elcock <melcock@scalable.com>
2024-08-17 19:32:11 +0100

    refactor: introduce react query

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -12,28 +12,10 @@ interface Config {
  * @throws {Error} If any of the required environment variables are missing.
  */
 export const getConfig = (): Config => {
-  if (!process.env.DISCORD_GUILD_ID) {
-    throw new Error("Missing environment variable: DISCORD_GUILD_ID");
-  }
-
-  if (!process.env.DISCORD_CHANNELS) {
-    throw new Error("Missing environment variable: DISCORD_CHANNELS");
-  }
-
-  if (!process.env.DISCORD_AUTHORIZATION_TOKEN) {
-    throw new Error(
-      "Missing environment variable: DISCORD_AUTHORIZATION_TOKEN"
-    );
-  }
-
-  if (!process.env.NODE_ENV) {
-    throw new Error("Missing environment variable: NODE_ENV");
-  }
-
   return {
-    guildId: process.env.DISCORD_GUILD_ID,
-    channelIds: process.env.DISCORD_CHANNELS.split(","),
-    authToken: process.env.DISCORD_AUTHORIZATION_TOKEN,
+    guildId: process.env.DISCORD_GUILD_ID || "",
+    channelIds: process.env.DISCORD_CHANNELS?.split(",") || [],
+    authToken: process.env.DISCORD_AUTHORIZATION_TOKEN || "",
     apiUrl: ["development", "test"].includes(process.env.NODE_ENV)
       ? "http://localhost:3000/api"
       : "https://titans-of-industry.vercel.app/api",
