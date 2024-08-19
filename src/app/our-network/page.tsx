b9c34910a0f2091896e9abd33be27618b1c5c691
Matt <matthew.elcock82@googlemail.com>
2024-08-19 22:15:03 +0100

    feat: filter by type (#21)
    Co-authored-by: Matt Elcock <melcock@scalable.com>

--- src/app/our-network/page.tsx
+++ src/app/our-network/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { FilterDropdown } from "@/components/FilterDropdown";
 import { OrgCard } from "@/components/OrgCard";
 import { Pagination } from "@/components/Pagination";
 import { useOrganisations } from "@/hooks/useOrganisations";
-import { Heading, Stack } from "@chakra-ui/react";
+import { Box, Heading, Stack } from "@chakra-ui/react";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import { Suspense } from "react";
 
@@ -21,38 +22,66 @@ function OurNetwork() {
   const searchParams = useSearchParams();
 
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+  const type = searchParams.get("type")
+    ? String(searchParams.get("type"))
+    : undefined;
 
   const { isLoading, error, data, pagination } = useOrganisations({
     pagination: { page },
+    filters: { type },
   });
 
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
-
-  if (!data || !pagination || error) {
-    if (error) console.error(error);
-    return <p>Error</p>;
-  }
+  const updateUrl = (params: string[]) => {
+    router.push(
+      `${pathname}${params.length > 0 ? `?${params.join("&")}` : ""}`
+    );
+  };
 
   const handleChangePage = (page: number) => {
-    router.push(`${pathname}?page=${page}`);
+    const type = searchParams.get("type");
+    const params = [`page=${page}`, ...(type ? [`type=${type}`] : [])];
+
+    updateUrl(params);
+  };
+
+  const handleTypeChange = (type: string[]) => {
+    const params = [...(type.length > 0 ? [`type=${type}`] : [])];
+
+    updateUrl(params);
   };
 
   return (
     <Stack spacing={5}>
       <Heading color="#E1E1E1">Our Network</Heading>
-      <Stack spacing={[5, 10]} direction={["column", "row"]} flexWrap="wrap">
-        {data.map(({ id, name, type }: any) => (
-          <OrgCard key={id} name={name} type={type} id={id} />
-        ))}
-      </Stack>
-      <Pagination
-        total={pagination.total}
-        limit={pagination.limit}
-        currentPage={page}
-        onChange={handleChangePage}
-      />
+      <Box display="flex" gap={5}>
+        <FilterDropdown
+          label="Type"
+          defaultOptions={type ? type.split(",") : undefined}
+          options={["Governorships", "Industries", "Powerbases"]}
+          onChange={handleTypeChange}
+        />
+      </Box>
+      {isLoading || !pagination || !data ? (
+        <p>Loading</p>
+      ) : (
+        <>
+          <Stack
+            spacing={[5, 10]}
+            direction={["column", "row"]}
+            flexWrap="wrap"
+          >
+            {data.map(({ id, name, type }: any) => (
+              <OrgCard key={id} name={name} type={type} id={id} />
+            ))}
+          </Stack>
+          <Pagination
+            total={pagination.total}
+            limit={pagination.limit}
+            currentPage={page}
+            onChange={handleChangePage}
+          />
+        </>
+      )}
     </Stack>
   );
 }
