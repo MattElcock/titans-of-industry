9a0a56ffb16c9ce6df18466e87887dd13233fb47
Matt <matthew.elcock82@googlemail.com>
2024-08-20 16:53:54 +0100

    refactor: page metadata (#26)
    * refactor: page metadata
    
    * refactor: extend metadata
    
    ---------
    
    Co-authored-by: Matt Elcock <melcock@scalable.com>

--- src/app/our-network/[orgId]/page.tsx
+++ src/app/our-network/[orgId]/page.tsx
@@ -5,9 +5,34 @@ import axios from "axios";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import { mdComponentsMap } from "./_mdComponentsMap";
+import { Metadata } from "next";
+import { commonOpenGraph } from "@/constants";
 
 interface Params {
   orgId: string;
+}
+
+export async function generateMetadata({ params }: { params: Params }) {
+  const config = getConfig();
+  const { orgId } = params;
+
+  const { data: organisation } = await axios.get(
+    `${config.apiUrl}/organisations/${orgId}`
+  );
+
+  const pageTitle = `${organisation.name} | Titans of Industry`;
+  const pageDescription = `Discover and connect with ${organisation.name}, part of the Titans of Industry roleplaying community supporting the Sith Empire in Star Wars: The Old Republic.`;
+
+  return {
+    title: pageTitle,
+    description: pageDescription,
+    openGraph: {
+      title: pageTitle,
+      description: pageDescription,
+      url: `https://titans-of-industry.vercel.app/our-network/${orgId}`,
+      ...commonOpenGraph,
+    },
+  };
 }
 
 export default async function Organisation({ params }: { params: Params }) {
