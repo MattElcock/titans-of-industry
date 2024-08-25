eb0ded85bb8b17dbac03d16b781060e83d9f229f
Matt Elcock <melcock@scalable.com>
2024-08-25 15:22:11 +0100

    chore: google site auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Box, Container } from "@chakra-ui/react";
 import { background } from "./_theme";
 import { Providers } from "./providers";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -14,6 +15,12 @@ export default function RootLayout({
   const gtmId = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="2aMRDzl58CR9m4SMhKekC6c2GL7V-7tNO3zJaxLUIl0"
+        />
+      </Head>
       {gtmId && <GoogleTagManager gtmId={gtmId} />}
       <body>
         <Providers>
