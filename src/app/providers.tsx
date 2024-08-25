fa5b7a6aa61e66fa2adabfc6c5e025d5d43fa72d
Matt Elcock <melcock@scalable.com>
2024-08-25 12:37:59 +0100

    refactor/cookie-consent

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,17 +3,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { theme } from "./_theme";
-import { CookieConsentProvider } from "@/context/CookieConsent";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
   return (
-    <CookieConsentProvider>
-      <ChakraProvider theme={theme}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </ChakraProvider>
-    </CookieConsentProvider>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ChakraProvider>
   );
 }
