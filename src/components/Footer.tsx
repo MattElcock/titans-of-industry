5e4923c75ae484c04266fe86addfba189180f22b
Matt Elcock <melcock@scalable.com>
2024-08-25 15:43:21 +0100

    refactor: tidy footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,6 @@
 import { Box, Container, Stack, Text } from "@chakra-ui/react";
 import { Map } from "lucide-react";
 import Link from "next/link";
-import { IoLogoVercel } from "react-icons/io5";
-import { RiCopyrightLine, RiGithubLine } from "react-icons/ri";
 
 export const Footer = () => {
   return (
@@ -27,41 +25,6 @@ export const Footer = () => {
             justifyContent={["space-between", "start"]}
             flexDirection={["column", "row"]}
           >
-            <Text fontSize="sm" display="flex" alignItems="center" gap={1}>
-              <IoLogoVercel aria-label="Vercel logo" />
-              Deployed on
-              <Link
-                href="https://vercel.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: "underline" }}
-              >
-                Vercel
-              </Link>
-            </Text>
-            <Text size="sm" display="flex" alignItems="center" gap={1}>
-              <RiGithubLine aria-label="GitHub logo" />
-              <Link
-                href="https://github.com/MattElcock/titans-of-industry"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: "underline" }}
-              >
-                View code on GitHub
-              </Link>
-            </Text>
-            <Text fontSize="sm" display="flex" alignItems="center" gap={1}>
-              <RiCopyrightLine aria-label="Copyright" />
-              Built by
-              <Link
-                href="https://www.linkedin.com/in/matthewelcock"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ textDecoration: "underline" }}
-              >
-                Matthew Elcock
-              </Link>
-            </Text>
             <Text fontSize="sm" display="flex" alignItems="center" gap={1}>
               <Map size={16} />
               <Link
